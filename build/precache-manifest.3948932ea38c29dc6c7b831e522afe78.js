4c2a2d1bc113f84e5803f892fc53721f6e01b8ad
pranav tomar <pranav.tomar.che17@itbhu.ac.in>
2020-05-22 18:23:49 +0530

    first commit

---
+++ build/precache-manifest.3948932ea38c29dc6c7b831e522afe78.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4beaf16297fd05a34e0bf88e3c0b08b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f97320fc62112637f56",
+    "url": "/static/css/main.3627b7fb.chunk.css"
+  },
+  {
+    "revision": "1d51633622ec55c39211",
+    "url": "/static/js/2.195cb782.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.195cb782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f97320fc62112637f56",
+    "url": "/static/js/main.fd482652.chunk.js"
+  },
+  {
+    "revision": "6a1aad1d7977ba72651b",
+    "url": "/static/js/runtime-main.237b01ea.js"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/static/media/icon-youtube.8499da12.svg"
+  }
+]);
